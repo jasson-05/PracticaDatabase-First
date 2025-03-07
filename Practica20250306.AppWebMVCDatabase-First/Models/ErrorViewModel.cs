bbf96ae2f46af9f3fd191d9ffcb602ca4a2ffac9
Jasso <Jasso@DESKTOP-97AP7FC>
2025-03-06 21:19:48 -0600

    Agregar archivos de proyecto.

---
+++ Practica20250306.AppWebMVCDatabase-First/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Practica20250306.AppWebMVCDatabase_First.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
