90fa51b8c313e76be63c8e0004a78dd7d6b8c0ee
Jasso <Jasso@DESKTOP-97AP7FC>
2025-03-07 10:43:05 -0600

    Cadena de conexion y archivo program modificado

--- Practica20250306.AppWebMVCDatabase-First/Program.cs
+++ Practica20250306.AppWebMVCDatabase-First/Program.cs
@@ -1,7 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+using Practica20250306.AppWebMVCDatabase_First.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<CatalogosDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("Conn"));
+});
+
 
 var app = builder.Build();
 
